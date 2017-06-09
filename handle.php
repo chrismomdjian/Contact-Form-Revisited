dc036bc5e3e61fbea7d0723287ca2c50629bfc3a
chrismomdjian <chrismomdjian@gmail.com>
2017-06-08 23:54:55 -0700

    php code update in handle.php

--- handle.php
+++ handle.php
@@ -1,23 +1,46 @@
 <?php
 
-function validateDate($date) {
-  $date = explode('/', $date);
+// function validateDate($date) {
+//   $date = explode('/', $date);
+//
+//   if(strlen($date[0]) != 2 // month
+//     || strlen($date[1]) != 2 // day
+//     || strlen($date[2]) != 4 // year
+//     || !checkdate($date[0], $date[1], $date[2])
+//   ) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+//
+// $my_date = "04/01/1994";
+//
+// if(validateDate($my_date)) {
+//   echo "You entered a valid date.";
+// } else {
+//   echo "Invalid date.";
+// }
 
-  if(strlen($date[0]) != 2 // month
-    || strlen($date[1]) != 2 // day
-    || strlen($date[2]) != 4 // year
-    || !checkdate($date[0], $date[1], $date[2])
-  ) {
-    return false;
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+  if(empty($_POST['full_name']) || empty($_POST['email']) || empty($_POST['message'])) {
+    $error_message = "Please fill out all fields in this form.";
   } else {
-    return true;
+    $name = clean_fields($_POST['full_name']);
+    $email = clean_fields(filter_var($_POST['email'], "FILTER_SANITIZE_EMAIL"));
+    $message = clean_fields($_POST['message']);
+
+    if(strlen($message) > 250) {
+      $error_message = "Your message is too long! The limit is 250 characters.";
+    }
   }
 }
 
-$my_date = "04/01/1994";
+function clean_fields($data) {
+  $data = htmlspecialchars($data);
+  $data = stripslashes($data);
+  $data = trim($data);
 
-if(validateDate($my_date)) {
-  echo "You entered a valid date.";
-} else {
-  echo "Invalid date.";
+  // Clean data!
+  return $data
 }
